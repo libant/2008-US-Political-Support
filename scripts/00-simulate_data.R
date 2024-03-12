97cf1f021de72b866874ad98c109d479fb4a422e
libant <liban.timir@mail.utoronto.ca>
2024-03-12 19:47:25 -0400

    simulating the data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the CES 2008 data
+# Author: Liban Timir
+# Date: 12 March 2024 
+# Contact: liban.timir@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
@@ -13,7 +12,27 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(555)
+
+num_obs <- 1000
+
+us_political_preferences <- tibble(
+  education = sample(0:4, size = num_obs, replace = TRUE),
+  gender = sample(0:1, size = num_obs, replace = TRUE),
+  support_prob = ((education + gender) / 5),
+) |>
+  mutate(
+    supports_obama = if_else(runif(n = num_obs) < support_prob, "yes", "no"),
+    education = case_when(
+      education == 0 ~ "< High school",
+      education == 1 ~ "High school",
+      education == 2 ~ "Some college",
+      education == 3 ~ "College",
+      education == 4 ~ "Post-grad"
+    ),
+    gender = if_else(gender == 0, "Male", "Female")
+  ) |>
+  select(-support_prob, supports_obama, gender, education)
 
 
 
